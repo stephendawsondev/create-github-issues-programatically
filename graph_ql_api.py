233a1488e49a45d865308fa26533d711eadf48da
Stephen Dawson <stephendawson@protonmail.com>
2024-03-19 21:51:44 +0000

    chore: remove unnecessary code from graphql

--- graph_ql_api.py
+++ graph_ql_api.py
@@ -10,6 +10,16 @@ HEADERS = {
     "Content-Type": "application/json",
 }
 
+# User stories and epics to add
+user_stories = [
+
+]
+
+epics = [
+
+]
+
+
 # Queries and Mutations
 create_issue_query = """
 mutation CreateIssue($input: CreateIssueInput!) {
@@ -19,66 +29,6 @@ mutation CreateIssue($input: CreateIssueInput!) {
       number
       title
     }
-  }
-}
-"""
-
-get_project_items_query = """
-{
-  node(id: "$projectId") {
-    ...on ProjectV2 {
-      items(first: 1) {
-        nodes {
-          id
-          content {
-            ... on Issue {
-              id
-              number
-              title
-            }
-          }
-          estimate: fieldValueByName(name: "Estimate") {
-            ...on ProjectV2ItemFieldNumberValue {
-              number
-              id
-            }
-          }
-          area: fieldValueByName(name: "Area") {
-            ...on ProjectV2ItemFieldTextValue {
-              text
-              id
-            }
-          }
-        }
-      }
-    }
-  }
-}
-""".replace("$projectId", env.PROJECT_ID)
-
-get_field_configs_query = """
-{
-  node(id: "$projectId") {
-    ...on ProjectV2 {
-      field(name: "Estimate") {
-        ... on ProjectV2Field {
-          id
-        }
-      }
-      field(name: "Area") {
-        ... on ProjectV2Field {
-          id
-        }
-      }
-    }
-  }
-}
-""".replace("$projectId", env.PROJECT_ID)
-
-clear_project_fields_query = """
-mutation ClearProjectV2ItemFieldValues($input: ClearProjectV2ItemFieldValuesInput!) {
-  clearProjectV2ItemFieldValues(input: $input) {
-    clientMutationId
   }
 }
 """
@@ -95,12 +45,10 @@ mutation UpdateProjectV2ItemFieldValue($input: UpdateProjectV2ItemFieldValueInpu
 def run_query(query, variables):
     request = requests.post(
         GRAPHQL_URL,
-        # This serializes the payload to JSON
         json={'query': query, 'variables': variables},
         headers=HEADERS
     )
     response = request.json()
-    print(json.dumps(response, indent=4))
     if request.status_code == 200 and 'errors' not in response:
         return response
     else:
@@ -144,27 +92,18 @@ def find_project_item_id(issue_id):
     """
 
     result = run_query(get_project_item_query, {})
-    print(f"Result: {result}")
-    if 'data' in result and 'node' in result['data'] and 'projectItems' in result['data']['node']:
+    if ('data' in result and 'node' in result['data']
+            and 'projectItems' in result['data']['node']):
         project_items = result['data']['node']['projectItems']['nodes']
         if project_items:
             project_item = project_items[0]
-
             return project_item['id']
 
     print("Error: No matching project item found for the issue.")
     return None, {}
 
 
-user_stories = [
-
-]
-
-epics = [
-
-]
-
-# Creating issues and updating project item fields
+# Create issues and update project fields
 for story in user_stories:
     body = f"""
 ### If applicable, add a related Epic.\n\n{story.get('related_epic', '')}\n
@@ -191,11 +130,10 @@ Epic: \n\n
     issue_result = run_query(create_issue_query, issue_variables)
     issue_id = issue_result["data"]["createIssue"]["issue"]["id"]
 
-    print("Waiting for 4 seconds...")
+    # delay for 2 seconds
     time.sleep(2)
-    print("4 seconds have passed.")
 
-    # Fetch the project item ID and field IDs
+    # Fetch the project item ID
     project_item_id = find_project_item_id(issue_id)
 
     if project_item_id:
@@ -220,9 +158,7 @@ Epic: \n\n
                 "input": {
                     "projectId": env.PROJECT_ID,
                     "itemId": project_item_id,
-                    # Ensure this is just the ID string
                     "fieldId": field_config_mapping[field_name],
-                    # Using the correctly formatted value
                     "value": formatted_value
                 }
             }
@@ -232,7 +168,7 @@ Epic: \n\n
 
 for epic in epics:
     body = f"""
-${epic.get('description', '')}
+{epic.get('description', '')}
 """.strip()
 
     issue_variables = {
@@ -250,32 +186,38 @@ ${epic.get('description', '')}
     issue_number = issue_result["data"]["createIssue"]["issue"]["number"]
     issue_title = issue_result["data"]["createIssue"]["issue"]["title"]
 
-    print("Waiting for 2 seconds...")
+    # delay for 2 seconds
     time.sleep(2)
-    print("2 seconds have passed.")
-    # Fetch the project item ID and field IDs
-    project_item_id, field_ids = find_project_item_id(issue_id)
+
+    # Fetch the project item ID
+    project_item_id = find_project_item_id(issue_id)
 
     if project_item_id:
+        # Mapping for field names to their respective env constants
+        field_config_mapping = {
+            "Estimate": env.ESTIMATE_FIELD_CONFIG_ID,
+            "Area": env.AREA_FIELD_CONFIG_ID,
+            "Importance": env.IMPORTANCE_FIELD_CONFIG_ID
+        }
         # Update custom fields for the project item
-        for field_name, field_value in [("Estimate", story["estimate"]), ("Area", story["area"])]:
-            if field_name in field_ids:
-                print(f"Updating field: {
-                      field_name} with value: {field_value}")
-                if field_name == "Estimate":
-                    value = {"number": field_value}  # For number fields
-                else:
-                    value = {"text": field_value}  # For text fields
+        for field_name, field_value in [("Estimate", epic["estimate"]), ("Area", epic["area"]), ("Importance", epic["importance"])]:
+            # Determine the type of value to update based on the field name
+            if field_name == "Estimate" or field_name == "Importance":
+                value_type = "number"
+            else:
+                value_type = "text"
 
-                    update_variables = {
-                        "input": {
-                            "projectId": env.PROJECT_ID,
-                            "itemId": project_item_id,
-                            "fieldId": field_ids[field_name],
-                            # Pass as a dictionary, not a string
-                            "value": {"text": field_value}
-                        }
-                    }
-                run_query(update_field_value_query, update_variables)
+            # Format the value correctly based on its type
+            formatted_value = {value_type: field_value}
+
+            update_variables = {
+                "input": {
+                    "projectId": env.PROJECT_ID,
+                    "itemId": project_item_id,
+                    "fieldId": field_config_mapping[field_name],
+                    "value": formatted_value
+                }
+            }
+            run_query(update_field_value_query, update_variables)
 
 print("All issues and their project fields have been created and updated.")
